f33b371f91fd71a6e35d96402c5fee06423e7d72
Hudson8811 <46067142+Hudson8811@users.noreply.github.com>
2021-05-13 13:45:13 +0300

    fixed

--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -1,5 +1,21 @@
 $(document).ready(function () {
    // $("input[name='phone']").mask(" +7 (999) 999-99-99");
-   // $('.single-item').slick();
+   $('.hero-slider').slick({
+      draggable: false,
+      arrows:true,
+      dots: true,
+      prevArrow:'<img class="hero-slider__prev" src="img/hero/arrow_left.png">',
+      nextArrow:'<img class="hero-slider__next" src="img/hero/arrow_right.png">',
+      dotsClass: 'hero-slider__pagination',
+      customPaging : function(slider, i) {
+         return '<div class="hero-slider__pagin"></div>';
+     },
+   });
+
+   try {
+      $('.hero-form__select').msDropDown();
+  } catch (e) {
+      alert(e.message);
+  }
 });
 
