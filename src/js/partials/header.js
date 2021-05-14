8d98317f6793787806d2881d32ad235ed19e9b8f
Hudson8811 <46067142+Hudson8811@users.noreply.github.com>
2021-05-14 19:21:33 +0300

    fixed

--- src/js/partials/header.js
+++ src/js/partials/header.js
@@ -3,6 +3,8 @@ $(document).ready(function () {
 	$('.sh-burger').click(function () {
 		$(this).toggleClass('sh-burger--active');
 		$('.sh-menu-wrap').toggleClass('sh-menu-wrap--open');
+		$('body').toggleClass('compensate-for-scrollbar')
 	});
+
 
 });
--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -1,22 +1,37 @@
 $(document).ready(function () {
-   // $("input[name='phone']").mask(" +7 (999) 999-99-99");
+   // $("input[name='phone']").mask(" +7 (999) 999-99-99")
    $('.hero-slider').slick({
       draggable: false,
       arrows:true,
       dots: true,
+      adaptiveHeight: true,
       prevArrow:'<button class="hero-slider__prev"><img src="img/hero/slider-arrow.svg"></button>',
       nextArrow:'<button class="hero-slider__next"><img src="img/hero/slider-arrow.svg"></button>',
       dotsClass: 'hero-slider__pagination',
       customPaging : function(slider, i) {
-         return '<div class="hero-slider__pagin"></div>';
+         return '<div class="hero-slider__pagin"></div>'
      },
    });
 
    try {
-      $('.hero-form__select').msDropDown();
+      $('.hero-form__select').msDropDown()
   } catch (e) {
-      alert(e.message);
+      console.log(e.message)
   }
-  // $("a.grouped_elements").fancybox();
+  const videos = $('.video__item-video iframe')
+  const videoImg = $('.video__item-img')
+  const videoInfo = $('.video__item-wrap')
+  const videoBtn = $('.video__button')
+
+  $(videoBtn).each(function(idx, item) {
+    $(item).on('click', () => {
+      console.log(idx);
+      const src = $(videos).eq(idx).attr('src')
+      $(videoBtn).eq(idx).css('display', 'none')
+      $(videoInfo).eq(idx).css('display', 'none')
+      $(videoImg).eq(idx).css('display', 'none')
+      $(videos).eq(idx).attr('src', src + '?autoplay=1');
+    })
+  })
 });
 
