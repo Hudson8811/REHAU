8327a0161d037573027f400a19b6a8cf95984a12
hudson88@yandex.ru <hudson88@yandex.ru>
2021-05-27 17:14:13 +0300

    refact

--- src/js/partials/header.js
+++ src/js/partials/header.js
@@ -4,8 +4,17 @@ $(document).ready(function () {
 		$(this).toggleClass('sh-burger--active');
 		$('.header-nav').toggleClass('header-nav--active');
 		$('.header-wrapper').toggleClass('header-wrapper--active');
-		$('body').toggleClass('compensate-for-scrollbar')
+		$('body').toggleClass('compensate-for-scrollbar');
+		$('.header-mobile-overlay').toggleClass('active');
 	});
 
-
+	$('.header-mobile-overlay').on('click', function() {
+		if ($('.header-mobile-overlay').hasClass('active')) {
+			$('.sh-burger').removeClass('sh-burger--active');
+			$('.header-nav').removeClass('header-nav--active');
+			$('.header-wrapper').removeClass('header-wrapper--active');
+			$('body').removeClass('compensate-for-scrollbar');
+			$('.header-mobile-overlay').removeClass('active');
+		}
+	})
 });
